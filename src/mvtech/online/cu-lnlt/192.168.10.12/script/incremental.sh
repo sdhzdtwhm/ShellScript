ef98e3126bfdef2ceea108d86d218a133e286286
YangHang <13716320887@139.com>
2017-07-05 10:40:58 +0800

    tessst

---
+++ src/mvtech/online/cu-lnlt/192.168.10.12/script/incremental.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#This script is backup mysql
+#The Author is mvtech by 2017
+
+#格式化日期
+today=$(date +%Y%m%d)
+echo "--------------------------------------------------------"
+echo "-------------------Today is $today----------------------"
+echo "--------------------Start backup------------------------"
+echo "--------------------------------------------------------"
+#定义变量
+lastDay=`date -d "-1 days" +%Y%m%d`
+baseDir="/mvtech/backup/file/$today"
+lastDir="/mvtech/backup/file/$lastDay"
+#创建备份文件夹
+mkdir -p $baseDir
+#增量备份
+/usr/bin/innobackupex --defaults-file=/etc/my.cnf --host=127.0.0.1 --user=root --password=mvtech123 --incremental $baseDir --no-timestamp --incremental-basedir=$lastDir --parallel=8 --throttle=800
+echo "---------删除30天以前的备份，时间为:`date +%Y%m%d`-------"
+find /mvtech/backup/file/ -name "*" -mtime +30 -exec rm -f {} \;
